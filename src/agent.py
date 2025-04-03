1c6052d467b87e268ad87a48ed50e694341c534a
cploujoux <cploujoux@blaxel.ai>
2025-04-03 16:30:42 +0200

    Initialize repository

--- src/agent.py
+++ src/agent.py
@@ -1,33 +1,37 @@
+# We have to apply nest_asyncio because crewai is not compatible with async
+import nest_asyncio
+
+nest_asyncio.apply()
+
 from typing import AsyncGenerator
 
 from blaxel.models import bl_model
 from blaxel.tools import bl_tools
-from langchain.tools import tool
-from langchain_core.messages import AIMessageChunk
-from langgraph.prebuilt import create_react_agent
+from crewai import Agent, Crew, Task
+from crewai.tools import tool
 
 
-@tool
+@tool("Weather")
 def weather(city: str) -> str:
     """Get the weather in a given city"""
     return f"The weather in {city} is sunny"
 
 async def agent(input: str) -> AsyncGenerator[str, None]:
-    prompt = "You are a helpful assistant that can answer questions and help with tasks."
-    tools = await bl_tools(["blaxel-search"]).to_langchain() + [weather]
-    model = await bl_model("gpt-4o-mini").to_langchain()
-    agent = create_react_agent(model=model, tools=tools, prompt=prompt)
-    messages = {"messages": [("user", input)]}
-    async for chunk in agent.astream(messages, stream_mode=["updates", "messages"]):
-        type_, stream_chunk = chunk
-        # This is to stream the response from the agent, filtering response from tools
-        if type_ == "messages" and len(stream_chunk) > 0 and isinstance(stream_chunk[0], AIMessageChunk):
-            msg = stream_chunk[0]
-            if msg.content:
-                if not msg.tool_calls:
-                   yield msg.content
-        # This to show a call has been made to a tool, usefull if you want to show the tool call in your interface
-        if type_ == "updates":
-            if "tools" in stream_chunk:
-                for msg in stream_chunk["tools"]["messages"]:
-                    yield f"Tool call: {msg.name}\n"
+    tools = await bl_tools(["blaxel-search"]).to_crewai() + [weather]
+    model = await bl_model("sandbox-openai").to_crewai()
+
+    agent = Agent(
+        role="Weather Researcher",
+        goal="Find the weather in a city",
+        backstory="You are an experienced weather researcher with attention to detail",
+        llm=model,
+        tools=tools,
+        verbose=True,
+    )
+    crew = Crew(
+        agents=[agent],
+        tasks=[Task(description="Find weather", expected_output=input, agent=agent)],
+        verbose=True,
+    )
+    result = crew.kickoff()
+    yield result.raw
